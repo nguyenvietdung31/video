7a3a5cabed209ea22d96a745b0caf1dfad962a61
Dung Nguyen <nguyen.vietdung.3108@gmail.com>
2022-10-31 13:38:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { Modal, List, Card, } from 'antd'
+import { useState } from 'react';
+import items from './data'
+import video from './data';
 
 function App() {
+  // const [videoItems, setVideoItems] = useState(items)
+  const [isOpen, setIsOpen] = useState(false)
+  const showModal = (index) => {
+    setIsOpen(true);
+    // const newVideo = items.filter((item)=> item.id === index);
+    // setVideoItems(newVideo)
+  };
+  const handleOk = () => {
+    setIsOpen(false);
+  };
+  const handleCancel = () => {
+    setIsOpen(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List
+        grid={{
+          gutter: 16,
+          column: 3,
+        }}
+        dataSource={items}
+        renderItem={(item) => (
+          <List.Item>
+            <Card title={item.title}>
+              <img src={item.img} width={'200px'} height={'200px'} alt=''
+                onClick={() => showModal(items)} />
+              <p>{item.desc}</p>
+              {/* <iframe key={item.id} width={600} height={450} src={item.link} frameBorder="0" allowFullScreen title={item.title}/> */}
+            </Card>
+          </List.Item>
+        )}
+      />
+      <Modal width={750} height={500} title={items.title} open={isOpen} onOk={handleOk} onCancel={handleCancel}>
+        {/* <iframe key={items.id} width={600} height={450} src={items.link} frameBorder="0" allowFullScreen title={items.title} destroyonclose='true'/> */}
+        {/* {
+          items.map((item,index)=>{
+            return (
+              <iframe key={index} src={item.link} width={600} height={450}
+              frameBorder="0" allowFullScreen title={item.title} destroyonclose='true'/>
+            )
+          })
+        } */}
+      </Modal>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,36 @@
+const video = [
+    {
+        id:0,
+        title: 'video01',
+        link: 'https://www.youtube-nocookie.com/embed/wcZqreTIhds',
+        img: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+        desc: 'aaaacascsac',
+    },
+    {
+        id:1,
+        title:'video02',
+        link:'https://www.youtube-nocookie.com/embed/eJyZ7k6lWfE',
+        img: 'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg',
+        desc:'saaaacascasc'  
+    },
+    {
+        id:2,
+        title: 'video03',
+        link: 'https://www.youtube-nocookie.com/embed/CHw1b_1LVBA',
+        img: 'https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg',
+        desc: '3333333333333',
+    },{
+        id:3,
+        title: 'video04',
+        link: 'https://www.youtube-nocookie.com/embed/UY4mIMlF2LA',
+        img: 'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
+        desc: '4444444444444444',
+    },{
+        id:4,
+        title: 'video05',
+        link: 'https://www.youtube-nocookie.com/embed/Y29OrOVJUKs',
+        img: 'https://static.remove.bg/remove-bg-web/ea3c274e1b7f6fbbfe93fad8b2b13d7ef352f09c/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
+        desc: '5555555555555',
+    },
+];
+export default video;
